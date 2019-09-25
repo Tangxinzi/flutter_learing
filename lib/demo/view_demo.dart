1bf14f914ab37b3a51128b33c2ba105558e97209
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-09-25 12:22:57 +0800

    GridView

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -2,34 +2,9 @@ import 'package:flutter/material.dart';
 import '../demo/post.dart';
 
 class ViewDemo extends StatelessWidget {
-  Widget _pageItemBuilder(BuildContext context, int index) {
-    return Stack(
-      children: <Widget>[
-        SizedBox.expand(
-          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
-        ),
-        Positioned(
-          bottom: 8.0,
-          left: 8.0,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(posts[index].title,
-                  style: TextStyle(fontWeight: FontWeight.bold)),
-              Text(posts[index].author),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      itemCount: posts.length,
-      itemBuilder: _pageItemBuilder,
-    );
+    return GridViewBuliderDemo();
   }
 }
 
@@ -63,6 +38,110 @@ class PageViewDemo extends StatelessWidget {
               style: TextStyle(fontSize: 32.0, color: Colors.white)),
         ),
       ],
+    );
+  }
+}
+
+class PageViewBuildDemo extends StatelessWidget {
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(posts[index].title,
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(posts[index].author),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+          color: Colors.grey[300],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'Item $index',
+            style: TextStyle(fontSize: 18.0, color: Colors.grey)
+          ),
+        );
+    });    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisCount: 3,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      children: _buildTiles(18),
+    );
+  }
+}
+
+class GridViewExtendDemo extends StatelessWidget {
+  List<Widget> _buildTiles(int length) {
+    return List.generate(length, (int index) {
+      return Container(
+          color: Colors.grey[300],
+          alignment: Alignment(0.0, 0.0),
+          child: Text(
+            'Item $index',
+            style: TextStyle(fontSize: 18.0, color: Colors.grey)
+          ),
+        );
+    });    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 150,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      children: _buildTiles(18),
+    );
+  }
+}
+
+class GridViewBuliderDemo extends StatelessWidget {
+  Widget _gridItemBuilder(BuildContext context, int index) {
+    return Container(
+      child: Image.network(posts[index].imageUrl, fit: BoxFit.cover),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: EdgeInsets.all(8.0),
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0
+      ),
     );
   }
 }
